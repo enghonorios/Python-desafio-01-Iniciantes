491b0505665d0b3247358ff0967971ecee5b3270
enghonorios <154471089+enghonorios@users.noreply.github.com>
2023-12-21 10:58:45 -0300

    Add files via upload

---
+++ Desafio 01.py
@@ -0,0 +1,9 @@
+
+print (' ====== Desafio 01 ===== ')
+nome= input ('Qual seu nome?')
+satisfação= 'Prazer em te conhecer'
+print (nome, satisfação)
+
+nome= input ('Qual seu nome?')
+satisfação= 'Prazer em te conhecer'
+print (nome, satisfação)
